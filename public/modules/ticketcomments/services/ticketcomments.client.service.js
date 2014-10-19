59e27fd5e66d62e5517bd33117a902ca51abd2fe
Terwase Kelvin Gberikon <terwase@live.com>
2014-10-19 08:25:16 +0100

    manually merging conflicts

--- public/modules/ticketcomments/services/ticketcomments.client.service.js
+++ public/modules/ticketcomments/services/ticketcomments.client.service.js
@@ -3,20 +3,12 @@
 //Ticketcomments service used to communicate Ticketcomments REST endpoints
 angular.module('ticketcomments').factory('Ticketcomments', ['$resource',
 	function($resource) {
-<<<<<<< HEAD
-		return $resource('ticketcomments/:ticketcommentId', { ticketcommentId: '@_id'
-=======
 		return $resource('tickets/:ticketId/comments/:commentId', { commentId: '@_id'
->>>>>>> devMean
 		}, {
 			update: {
 				method: 'PUT'
 			}
 		});
 	}
-<<<<<<< HEAD
-]);
-=======
 ]);
 
->>>>>>> devMean
